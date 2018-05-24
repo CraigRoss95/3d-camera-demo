3b7a3248f1461929de2feb08bd4fefcc7aa3e077
ODIN-PC\ODIN <craigross.emailme@gmail.com>
2018-05-24 16:13:32 -0400

    Initial commit

---
+++ Assets/cameraControler.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControler : MonoBehaviour {
+public Transform lookAt;
+public Transform camTransfrom;
+public float camSpeed;
+private float mouseX = 0.0f;
+private float mouseY = 0.0f;
+
+private Camera cam;
+private float tempDistance;
+private float distance = 10.0f;
+private float sensitivityX = 4.0f;
+private float sensitivityY = 1.0f;
+private float distanceOffset = 0.0f;
+private float zoomSpeed = 0.1f;
+private float maxZoom = 10.0f;
+private bool canZoom;
+private float userZoom;
+private float toWall;
+
+	// Use this for initialization
+	void Start () {
+		canZoom = true;
+		camTransfrom = transform;
+		cam = Camera.main;
+		distance = maxZoom;
+		userZoom = maxZoom;
+		toWall = 11.0f;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	
+	}
+	void LateUpdate()
+	{
+		Debug.Log("can zoom = " + canZoom);
+		mouseX = mouseX + Input.GetAxis("Mouse X") * sensitivityX;
+		mouseY = mouseY + Input.GetAxis("Mouse Y") * sensitivityY;
+		mouseY = Mathf.Clamp (mouseY, -20, 75);
+	//zooming in or out if the camera is not aggenst a wall
+	if( toWall >= userZoom)
+	{
+		userZoom = userZoom - (Input.GetAxis("Mouse ScrollWheel"));
+		userZoom = Mathf.Clamp(userZoom, 1.0f, 10.0f);
+		distance = userZoom;
+	}
+	//zooming in while the camera is agenst a wall
+	else if(toWall < userZoom && Input.GetAxis("Mouse ScrollWheel") > 0)
+	{
+		//Debug.Log("your good!");
+		userZoom = toWall;
+	}
+
+
+	//Debug.Log("userzoom = " + userZoom);
+		Zoom();
+		Vector3 dir = new Vector3 (0, 0, -distance);
+		Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0);
+
+		camTransfrom.position = Vector3.MoveTowards(camTransfrom.position, lookAt.position + rotation*dir, camSpeed * Time.deltaTime );
+
+		transform.LookAt(lookAt);
+		lookAt.transform.LookAt(transform);
+	}
+	private void Zoom()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(lookAt.transform.position,lookAt.transform.TransformDirection(Vector3.forward), out hit, maxZoom))
+		{
+			toWall = hit.distance;
+			if(hit.distance < userZoom)
+			{
+				//Debug.Log(" hit distance: " + hit.distance);
+				distance = distance - camSpeed;
+				Mathf.Clamp(distance, 1.0f, hit.distance);
+				distance = hit.distance;
+			}
+			else 
+			{
+				distance = userZoom;
+			}
+		}
+		else 
+		{
+			toWall = Mathf.Infinity;
+		}
+		//Debug.Log("distance offset " + distanceOffset);
+		
+			
+	}
+}
+
---
+++ Assets/playerControler.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour {
+	public Transform cam;
+	public float velocity;
+	public float turningSpeed;
+	public LayerMask ground;
+
+	private bool grounded;
+	private float height = 1.0f;
+	private float heightBuffer = 0.1f;
+	public float maxAngle = 160.0f;
+	public GameObject playerBody;
+	private RaycastHit infront;
+	private bool wallFound;
+
+
+
+
+	private Vector2 input;
+
+	private float angle;
+	private float groundAngle;
+	private Quaternion targetRotation;
+
+	private Vector3 forward;
+	private Vector3 forwardLeft;
+	private Vector3 forwardRight;
+	private RaycastHit hitInfo;
+	private Transform sphereCastHit;
+	
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{	
+
+		GetInput();
+		CalculateDirection();
+		CalculateForward();
+		CalculateGroundAngle();
+		CheckGround();
+		CastCircle();
+		ApplyGravity();
+		Lines();
+
+		if(Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1)return;
+
+		Rotate();
+		Move();
+
+
+	}
+
+	void GetInput()
+	{
+		input.x = Input.GetAxisRaw("Horizontal");
+		input.y = Input.GetAxisRaw("Vertical");
+	}
+
+	void CalculateDirection()
+	{
+		angle = Mathf.Atan2(input.x, input.y);
+		angle = Mathf.Rad2Deg * angle;
+		angle = angle + cam.eulerAngles.y;
+
+	}
+
+	void Rotate()
+	{
+		targetRotation = Quaternion.Euler(0,angle,0);
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turningSpeed* Time.deltaTime);
+
+
+		playerBody.transform.eulerAngles = new Vector3 ( - Vector3.Angle(hitInfo.normal, transform.forward) +90,angle, 0.0f);
+
+
+		//playerBody.transform.rotation = Quaternion.AngleAxis (  transform.rotation.y, Vector3.up);
+	
+
+	}
+
+	void Move()
+	{if(groundAngle >= maxAngle) return;
+		if(wallFound == false)
+		{
+		transform.position = transform.position +(forward * velocity * Time.deltaTime);
+		}
+	}
+
+	void CalculateForward()
+	{
+		if(!grounded)
+		{
+			forward = transform.forward;
+			return;
+		}
+
+		forward = Vector3.Cross(hitInfo.normal, -transform.right);
+		forwardLeft =  Vector3.Cross(hitInfo.normal, transform.forward - transform.right);
+		forwardRight =  Vector3.Cross(hitInfo.normal, -transform.forward -transform.right);
+		
+
+	}
+	void CalculateGroundAngle()
+	{
+		if(!grounded)
+		{
+			groundAngle = 90;
+			return;
+		}
+		groundAngle = Vector3.Angle(hitInfo.normal, transform.forward);
+		if (groundAngle < Vector3.Angle(hitInfo.normal,transform.forward + transform.right))
+		{
+			groundAngle = Vector3.Angle(hitInfo.normal, transform.forward + transform.right);
+		}
+		if (groundAngle < Vector3.Angle(hitInfo.normal, transform.forward - transform.right))
+		{
+			groundAngle = Vector3.Angle(hitInfo.normal, transform.forward - transform.right);
+		}
+		if (groundAngle < Vector3.Angle(hitInfo.normal, transform.right))
+		{
+			groundAngle = Vector3.Angle(hitInfo.normal, transform.right);
+		}
+		if (groundAngle < Vector3.Angle(hitInfo.normal, -transform.right))
+		{
+			groundAngle = Vector3.Angle(hitInfo.normal, -transform.right);
+		}
+	}
+	void CheckGround()
+	{
+		if(Physics.Raycast(transform.position, -Vector3.up, out hitInfo, height + heightBuffer, ground))
+		{
+			if(Vector3.Distance(transform.position,hitInfo.point) < height)
+			{
+				transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.up * height , 5 * Time.deltaTime);
+			}
+			grounded = true;
+		}
+		else
+		{
+			grounded = false;
+		}
+		Debug.Log("grounded = " + grounded);
+
+	}
+	
+	void ApplyGravity()
+	{
+		if(!grounded)
+		{
+			transform.position = transform.position + Physics.gravity * Time.deltaTime;
+		}
+	}
+	
+	void CastCircle()
+	{
+
+	}
+
+	void Lines()
+	{
+		Debug.DrawLine(transform.position,transform.position+ forward *height * 2, Color.blue);
+		Debug.DrawLine(transform.position,transform.position +  forwardLeft *height * 2, Color.red);
+		Debug.DrawLine(transform.position,transform.position +  forwardRight *height * 2, Color.red);
+		//Debug.DrawLine(transform.position, transform.position - Vector3.up * height, Color.green);
+		if(Physics.Raycast(transform.position, forward, out infront, 0.6f ,ground))
+		{
+			wallFound = true;
+			Debug.Log("infront = " + infront.normal);
+		}
+		else if(Physics.Raycast(transform.position, forwardLeft, out infront, 0.6f ,ground))
+		{
+			wallFound = true;
+		}
+		else if(Physics.Raycast(transform.position, forwardRight, out infront, 0.6f ,ground))
+		{
+			wallFound = true;
+		}
+
+		else if(Physics.Raycast(transform.position+ new Vector3(0,height - heightBuffer, 0), forward, out infront, 0.6f ,ground))
+		{
+			wallFound = true;
+			Debug.Log("infront = " + infront.normal);
+		}
+		else if(Physics.Raycast(transform.position + new Vector3(0,height + heightBuffer, 0), forwardLeft, out infront, 0.6f ,ground))
+		{
+			wallFound = true;
+		}
+		else if(Physics.Raycast(transform.position+ new Vector3(0,height - heightBuffer, 0), forwardRight, out infront, 0.6f ,ground))
+		{
+			wallFound = true;
+		}
+
+		else if(Physics.Raycast(transform.position+ new Vector3(0,-height - heightBuffer, 0), forward + new Vector3(0.0f,30.0f,0.0f), out infront, 0.3f ,ground))
+		{
+			wallFound = true;
+			Debug.Log("infront = " + infront.normal);
+			Debug.DrawLine( transform.position+ new Vector3(0,-height - heightBuffer, 0),infront.point, Color.red);
+		}
+		else if(Physics.Raycast(transform.position + new Vector3(0,-height - heightBuffer, 0), forwardLeft+ new Vector3(0.0f,30.0f,0.0f), out infront, 0.3f ,ground))
+		{
+			wallFound = true;
+		}
+		else if(Physics.Raycast(transform.position+ new Vector3(0,-height - heightBuffer, 0), forwardRight+ new Vector3(0.0f,30.0f,0.0f), out infront, 0.3f ,ground))
+		{
+			wallFound = true;
+		}
+		else
+		{
+		wallFound = false;
+		}
+	}
+
+
+
+}
