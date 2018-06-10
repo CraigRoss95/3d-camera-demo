5edaa22e4a50380198dec1a2c5fce3c863186193
craig's laptop <craigross.emailme@gmail.com>
2018-06-10 00:03:16 -0400

    the light switch update
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death
    life
    death

---
+++ Assets/scripts/lightSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightSwitch : MonoBehaviour {
+
+public GameObject sun;
+public GameObject playerLightSource;
+public GameObject playerCamLightSource;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.GetComponent<toggle>().GetOn())
+		{
+			sun.SetActive(true);
+			playerLightSource.SetActive(false);
+			playerCamLightSource.SetActive(false);
+			RenderSettings.ambientIntensity = 1.0f;
+			RenderSettings.reflectionIntensity = 1.0f;
+
+		}
+		else
+		{
+			sun.SetActive(false);
+			playerLightSource.SetActive(true);
+			playerCamLightSource.SetActive(true);
+			RenderSettings.ambientIntensity = 0.0f;
+			RenderSettings.reflectionIntensity = 0.0f;
+		}
+		
+	}
+}
